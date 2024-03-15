ecbaff35facc1732c4ab03932e09246d9022cd4a
Alex <arsenntv@gmail.com>
2024-03-15 07:21:59 -0400

    Merged Edwin's branch

---
+++ MealZero/MealZero/CoachController.swift
@@ -0,0 +1,16 @@
+//
+//  CoachController.swift
+//  MealZero
+//
+//  Created by Alex Arsentev on 2024-03-15.
+//
+
+import UIKit
+
+class CoachController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
